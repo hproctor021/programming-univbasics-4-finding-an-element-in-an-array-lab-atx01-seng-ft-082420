d3cc27deca272f87327550eaba721f8482bd628f
Haley Proctor <hproctor021@gmail.com>
2020-07-30 18:45:09 +0000

    Automatically backed up by Learn

--- lib/find_in_array.rb
+++ lib/find_in_array.rb
@@ -2,6 +2,7 @@ def find_element_index(array, value_to_find)
   counter = 0
   
   while counter < array.length do
+    while 
     result = array[counter] == value_to_find
     if result == true
       return result[index]
